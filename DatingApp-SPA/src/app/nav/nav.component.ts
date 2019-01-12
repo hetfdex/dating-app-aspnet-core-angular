1e40600881947a11bab08622d03389f06caabbb9
Jose Pereira <hetfdex@gmail.com>
2019-01-12 18:29:01 +0000

    Error handling implemented

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Login: Success');
     }, error => {
-      console.log('Login: Fail');
+      console.log(error);
     });
   }
 
